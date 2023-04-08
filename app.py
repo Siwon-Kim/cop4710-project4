15c5522eb602b16eda587ac69a066b9b1b202fb2
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-08 18:10:39 -0400

    add requirements.txt

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# BE - server side using Flask
+# server side using Flask
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+# to keep confidential data in secret
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+GCP_PROJECT_ID = os.getenv('MONGO_DB_STORAGE')
+
+# connect to MongoDB database
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://siwon:rlaznf11@cluster0.icysouv.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsiwon
+
+
+# main page
+@app.route('/')
+def home():
+    return render_template('index.html')
