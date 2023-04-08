bd78f655a4541b1448dba4850cfebebc3ffb1246
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-08 18:32:35 -0400

    change DB storage name

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
-GCP_PROJECT_ID = os.getenv('MONGO_DB_STORAGE')
+MONGO_DB_STORAGE = os.getenv('MONGO_DB_STORAGE')
 
 # connect to MongoDB database
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://siwon:rlaznf11@cluster0.icysouv.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsiwon
+client = MongoClient(MONGO_DB_STORAGE)
+db = client.dbProject4
 
 
-# main page
+# connect to FE: main client page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('client.html')
